fef7deab34ec4ac62e03be51825a17edc6e0404e
Kevin <406972552@qq.com>
2023-05-10 09:52:18 +0800

    添加了执行文件和窗口图标

--- build.rs
+++ build.rs
@@ -1,3 +1,10 @@
+// extern crate winres;
+
 fn main() {
+    if cfg!(target_os = "windows") {
+        let mut res = winres::WindowsResource::new();
+        res.set_icon("image/img.ico");
+        res.compile().unwrap();
+    }
     slint_build::compile("ui/appwindow.slint").unwrap();
 }
